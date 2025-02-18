89c4fca959dee8daed549ba694f804abf8f42e87
Virgilio20-hub <joaovirgilio067@gmai.com>
2025-02-18 17:03:47 -0400

    Primeiro

---
+++ Aula 3.py
@@ -0,0 +1,72 @@
+print("Hello, world!")
+a = 5
+b = 2
+c = "?"
+Resultado = ("c =")
+print(Resultado, a + b)
+print(Resultado, a - b)
+print(Resultado, a * b)
+print(Resultado, a / b)
+print(Resultado, a // b)
+print(Resultado, a % b)
+print(Resultado, a ** b)
+x = 5
+y = 10
+Resposta = ("z =")
+print(Resposta, x == y) , print(Resposta, x != y) , print(Resposta, x < y) , print(Resposta, x > y)
+a = True
+b = False
+Resposta = ("C =")
+print(Resultado, a and b ) , print(Resultado, a or b) , print(Resultado, not a)
+lista1 = [1, 2, 3]
+lista2 = [1, 2, 3]
+Resultado = ("list = ")
+print(Resultado, lista1 is lista2), print(Resultado, lista1 is not lista2)
+Sequencia = [1, 2, 3, 4]
+Resposta = ("sequence = ")
+print(Resposta, 3 in Sequencia), print(Resposta, 5 not in Sequencia)
+Nome = "João Virgilio"
+Idade = 20
+print("Nome =", Nome, "Idade =", Idade)
+print("Python", "é", "divertido", sep= "-", end= "!\n")
+palavra1 = "olá, "
+palavra2 = "mundo!"
+resultado = palavra1 + palavra2
+print(resultado)
+nome = "João Virgilio" 
+mensagem = "Bem-vindo, " + nome + "!"
+print(mensagem)
+lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+resultado_lista = lista1 + lista2
+Resposta = ("resultado =")
+print(Resposta, lista1 + lista2)
+lista3 = ["7, 8, 9"]
+resultado_lista += lista3
+Resposta = ("resultado =")
+print(Resposta, resultado_lista)
+numero = 42
+numero_ponto_flutuante = 3.14
+resultado = numero + numero_ponto_flutuante
+Resposta = ("resultado =")
+print(Resposta, resultado)
+numero_string = "42"
+numero_inteiro = int(numero_string)
+Resposta = ("resutado =")
+print(Resposta, numero_inteiro)
+numero_inteiro = 10
+condição = (numero_inteiro > 5)
+Resposta = ("resultado =")
+print(Resposta, condição)
+a = 5
+b = 2
+c = "?"
+Resultado = ("c =")
+print(Resultado, a + b)
+print(Resultado, a - b)
+print(Resultado, a * b)
+print(Resultado, a / b)
+print(Resultado, a // b)
+print(Resultado, a % b)
+print(Resultado, a ** b)
+print(Resultado, a + b, (a - b) )
